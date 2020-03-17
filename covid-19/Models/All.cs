1a4c677bdda06d644f21766b40b060d2ce4bc8fa
Martin Georgiev <mggeorgiev@gmail.com>
2020-03-17 10:01:04 +0200

    Add project files.

---
+++ covid-19/Models/All.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace covid_19.Models
+{
+    public class All
+    {
+        public int Cases { get; set; }
+        public int Deaths { get; set; }
+        public int Recovered { get; set; }
+    }
+}
---
+++ covid-19/Models/Country.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace covid_19.Models
+{
+    public class Country
+    {
+        public string Country { get; set; }
+        public int Cases { get; set; }
+        public int TodayCases { get; set; }
+        public int Deaths { get; set; }
+        public int TodayDeaths { get; set; }
+        public int Recovered { get; set; }
+        public int Active { get; set; }
+        public int Critical { get; set; }
+    }
+}
