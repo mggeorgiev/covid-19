e53e5f11f167dee2ba7ff0db345050c86daba821
Martin Georgiev <mggeorgiev@gmail.com>
2020-04-02 10:16:35 +0300

    Refactor

--- covid-19/Controllers/api/AllController.cs
+++ covid-19/Controllers/api/AllController.cs
@@ -22,8 +22,6 @@ namespace covid_19.Controllers
             _context = context;
         }
 
-
-
         // GET: api/All
         [HttpGet]
         public async Task<ActionResult<IEnumerable<All>>> GetAll()
@@ -31,48 +29,48 @@ namespace covid_19.Controllers
             return await _context.All.ToListAsync();
         }
 
-        public static async void GetAllFromHeraku(string endpoint)
-        {
-            //Define your baseUrl
-            string baseUrl = "https://coronavirus-19-api.herokuapp.com/";
-            //Have your using statements within a try/catch blokc that will catch any exceptions.
-            try
-            {
-                //HttpClient client_veni = new HttpClient();
-                //We will now define your HttpClient with your first using statement which will use a IDisposable.
-                using (HttpClient client = new HttpClient())
-                {
-                    //In the next using statement you will initiate the Get Request, use the await keyword so it will execute the using statement in order.
-                    //The HttpResponseMessage which contains status code, and data from response.
-                    using (HttpResponseMessage res = await client.GetAsync(baseUrl + endpoint))
-                    {
-                        //Then get the data or content from the response in the next using statement, then within it you will get the data, and convert it to a c# object.
-                        using (HttpContent content = res.Content)
-                        {
-                            //Now assign your content to your data variable, by converting into a string using the await keyword.
-                            var data = await content.ReadAsStringAsync();
-                            //If the data isn't null return log convert the data using newtonsoft JObject Parse class method on the data.
-                            if (data != null)
-                            {
-                                //Now log your data in the console
-                                Console.WriteLine("data------------{0}", data);
+        //public static async void GetAllFromHeraku(string endpoint)
+        //{
+        //    //Define your baseUrl
+        //    string baseUrl = "https://coronavirus-19-api.herokuapp.com/";
+        //    //Have your using statements within a try/catch blokc that will catch any exceptions.
+        //    try
+        //    {
+        //        //HttpClient client_veni = new HttpClient();
+        //        //We will now define your HttpClient with your first using statement which will use a IDisposable.
+        //        using (HttpClient client = new HttpClient())
+        //        {
+        //            //In the next using statement you will initiate the Get Request, use the await keyword so it will execute the using statement in order.
+        //            //The HttpResponseMessage which contains status code, and data from response.
+        //            using (HttpResponseMessage res = await client.GetAsync(baseUrl + endpoint))
+        //            {
+        //                //Then get the data or content from the response in the next using statement, then within it you will get the data, and convert it to a c# object.
+        //                using (HttpContent content = res.Content)
+        //                {
+        //                    //Now assign your content to your data variable, by converting into a string using the await keyword.
+        //                    var data = await content.ReadAsStringAsync();
+        //                    //If the data isn't null return log convert the data using newtonsoft JObject Parse class method on the data.
+        //                    if (data != null)
+        //                    {
+        //                        //Now log your data in the console
+        //                        Console.WriteLine("data------------{0}", data);
 
-                            }
-                            else
-                            {
-                                Console.WriteLine("NO Data----------");
-                            }
+        //                    }
+        //                    else
+        //                    {
+        //                        Console.WriteLine("NO Data----------");
+        //                    }
 
-                        }
-                    }
-                }
-            }
-            catch (Exception exception)
-            {
-                Console.WriteLine("Exception Hit------------");
-                Console.WriteLine(exception);
-            }
-        }
+        //                }
+        //            }
+        //        }
+        //    }
+        //    catch (Exception exception)
+        //    {
+        //        Console.WriteLine("Exception Hit------------");
+        //        Console.WriteLine(exception);
+        //    }
+        //}
 
         // GET: api/All/5
         [HttpGet("{id}")]
@@ -126,47 +124,12 @@ namespace covid_19.Controllers
         [HttpPost]
         public async Task<ActionResult<All>> PostAll()
         {
-            //All all = null;
-            //HttpClient client = new HttpClient();
-            //HttpResponseMessage response = await client.GetAsync(path);
-
-            //if (response.IsSuccessStatusCode)
-            //{
-            //    //var temp = await response.Content.ReadAsStringAsync<All>();
-            //}
-            //return all;
 
             All all = new All();
-            //GetAllFromHeraku("all");
-            //all = null;
 
-            HttpClient client = new HttpClient();
-            //HttpResponseMessage response = await client.GetAsync(path);
+            all = GetAllFromHeraku("");
 
-            client.BaseAddress = new Uri("https://coronavirus-19-api.herokuapp.com/");
-            //HTTP GET
-
-
-            var responseTask = client.GetAsync("all");
-            responseTask.Wait();
-            var content = responseTask.Result.Content.ReadAsStringAsync().Result;
-
-            if (responseTask.Result.IsSuccessStatusCode)
-            {
-                allDTO alls = JsonConvert.DeserializeObject<allDTO>(content);
-                
-                all.Cases = alls.cases;
-                all.Deaths = alls.deaths;
-                all.Recovered = alls.recovered;
-                all.Date = DateTime.Now;
-            }
-            else //web api sent error response 
-            {
-                //log response status here..
-
-                return all;
-            }
-
+            if (all != null )
 
             _context.All.Add(all);
             await _context.SaveChangesAsync();
@@ -190,6 +153,39 @@ namespace covid_19.Controllers
             return all;
         }
 
+        private All GetAllFromHeraku(string BaseAddress)
+        {
+            All all = new All();
+            //Martin: this will overuse the server's resources
+            HttpClient client = new HttpClient();
+
+            //HTTP GET
+            if(BaseAddress == "")
+                client.BaseAddress = new Uri("https://coronavirus-19-api.herokuapp.com/");
+
+            var responseTask = client.GetAsync("all");
+            responseTask.Wait();
+
+
+            var content = responseTask.Result.Content.ReadAsStringAsync().Result;
+
+            if (responseTask.Result.IsSuccessStatusCode)
+            {
+                allDTO alls = JsonConvert.DeserializeObject<allDTO>(content);
+
+                all.Cases = alls.cases;
+                all.Deaths = alls.deaths;
+                all.Recovered = alls.recovered;
+                all.Date = DateTime.Now;
+
+                return all;
+            }
+            else //web api sent error response 
+            {
+                //log response status here..
+                return all;
+            }
+        }
         private bool AllExists(int id)
         {
             return _context.All.Any(e => e.Id == id);
