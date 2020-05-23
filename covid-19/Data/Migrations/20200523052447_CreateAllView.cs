f3d8886badebdd4bdfddf00ab8740e07990103bf
Martin Georgiev <mggeorgiev@gmail.com>
2020-05-23 08:41:16 +0300

    Create AllView

---
+++ covid-19/Data/Migrations/20200523052447_CreateAllView.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace covid_19.Data.Migrations
+{
+    public partial class CreateAllView : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("CREATE VIEW[AllUnique] AS " +
+                                        "SELECT* FROM[dbo].[All] " +
+                                        "WHERE [Date] in " +
+                                        "(SELECT TOP (1000) " +
+                                                "Max([Date]) " +
+                                                "FROM[covid19].[dbo].[All] " +
+                                                "GROUP BY YEAR([Date]) " +
+                                                ",Month([Date]) " +
+                                                ",DAy([Date]))" +
+                                                "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP VIEW [dbo].[AllUnique]");
+        }
+    }
+}
