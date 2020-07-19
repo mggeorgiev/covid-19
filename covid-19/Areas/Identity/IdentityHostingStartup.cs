1e7448d321a8d6217bcf138ef2ed8e8e681d65a1
Martin Georgiev <mggeorgiev@gmail.com>
2020-07-19 21:20:06 +0200

    Add scafolding for ASP identity

---
+++ covid-19/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using covid_19.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(covid_19.Areas.Identity.IdentityHostingStartup))]
+namespace covid_19.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
